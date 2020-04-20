352c4359966b6d3f1ad4b9f23f773e5a6dc586f2
gamorim95 <gamorim@yoobot.com.br>
2020-04-20 15:26:08 -0300

    Finishing up the weather card

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import WeatherCard from "./components/WeatherCard/component";
 function App() {
   return (
     <div className="App">
-      <WeatherCard temp={-5} />
-      <WeatherCard temp={15} />
-      <WeatherCard temp={40} />
+      <WeatherCard temp={-12} condition="Rain" city="Sydney" country="AU" />
+      <WeatherCard temp={15} condition="Dust" city="Itajubá" country="BR" />
+      <WeatherCard temp={40} condition="Tornado" city="Lorena" country="BR" />
     </div>
   );
 }
--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -5,22 +5,22 @@ import Location from "./Location";
 import Condition from "./Condition";
 import Icon from "./Icon";
 
-const WeatherCard = (props) => {
+const WeatherCard = ({ temp, condition, city, country }) => {
   let highColor = 0;
   let lowColor = 0;
   let bg = null;
-  if (props.temp > 12) {
+  if (temp > 12) {
     // This is for hot weather
-    highColor = (1 - (props.temp - 12) / 28) * 255;
+    highColor = (1 - (temp - 12) / 28) * 255;
     lowColor = highColor - 150;
     bg = `linear-gradient(
       to top,
       rgb(255, ${highColor}, 0),
       rgb(255, ${lowColor}, 0)
     )`;
-  } else if (props.temp <= 12) {
+  } else if (temp <= 12) {
     // This is for cold weather
-    highColor = (1 - (props.temp + 20) / 32) * 255;
+    highColor = (1 - (temp + 20) / 32) * 255;
     lowColor = highColor - 150;
     bg = `linear-gradient(
       to top,
@@ -31,7 +31,7 @@ const WeatherCard = (props) => {
 
   const Card = styled.div`
     margin: 0 auto;
-    background: ${bg}};
+    background: ${bg};
     width: 200px;
     height: 240px;
     display: flex;
@@ -42,9 +42,9 @@ const WeatherCard = (props) => {
   `;
   return (
     <Card>
-      <Location />
-      <Icon />
-      <Condition />
+      <Location city={city} country={country} />
+      <Icon condition={condition} />
+      <Condition temp={temp} condition={condition} />
     </Card>
   );
 };
