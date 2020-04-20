d3fffee3c0383a8069dc8864d99ea1f5befc3e7f
gamorim95 <gamorim@yoobot.com.br>
2020-04-20 11:54:15 -0300

    Stateless Weather Card

---
+++ src/components/WeatherCard/Icon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Icon = () => {
+  const Icon = styled.img`
+    width: 40%;
+  `;
+
+  return (
+    <Icon
+      className="icon"
+      src="./img/Mostly Cloudy-2x.png"
+      alt="Weather Icon"
+    />
+  );
+};
+
+export default Icon;
