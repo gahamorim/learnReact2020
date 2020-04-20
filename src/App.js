a3caa6738966a398fe4a631a993ddb771f4ddf9c
gamorim95 <gamorim@yoobot.com.br>
2020-04-20 16:06:08 -0300

    Understanding and handeling API requests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=a888c6d89d8ef6c45ff1eb3610fe307b"
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+
+  data().then((res) => {
+    console.log("The main temperature is " + res.main.temp);
+    console.log("The feels like temperature is " + res.main.feels_like);
+  });
+
   return (
     <div className="App">
       <WeatherCard temp={-12} condition="Rain" city="Sydney" country="AU" />
