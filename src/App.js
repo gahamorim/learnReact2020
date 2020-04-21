264f6ced7a1185b014f546cd015563045f2ffea5
gamorim95 <gamorim@yoobot.com.br>
2020-04-21 10:05:38 -0300

    Improving the weather code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import WeatherCard from "./components/WeatherCard/component";
+import WeatherEngine from "./components/WeatherEngine";
 
 function App() {
-  const data = async () => {
-    const apiRes = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=a888c6d89d8ef6c45ff1eb3610fe307b"
-    );
-    const resJSON = await apiRes.json();
-    return resJSON;
-  };
-
-  data().then((res) => {
-    console.log("The main temperature is " + res.main.temp);
-    console.log("The feels like temperature is " + res.main.feels_like);
-  });
-
   return (
     <div className="App">
-      <WeatherCard temp={-12} condition="Rain" city="Sydney" country="AU" />
-      <WeatherCard temp={15} condition="Dust" city="Itajubá" country="BR" />
-      <WeatherCard temp={40} condition="Tornado" city="Lorena" country="BR" />
+      <WeatherEngine location="sydney,au" />
+      <WeatherEngine location="melbourne,au" />
     </div>
   );
 }
---
+++ src/components/WeatherEngine.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+import WeatherCard from "./WeatherCard/component";
+
+const WeatherEngine = ({ location }) => {
+  //init for our state variables
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
+
+  //defining the data fetching function
+  const getWeather = async (q) => {
+    const apiRes = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=a888c6d89d8ef6c45ff1eb3610fe307b`
+    );
+    const resJSON = await apiRes.json();
+    setWeather({
+      temp: resJSON.main.temp,
+      city: resJSON.name,
+      condition: resJSON.weather[0].main,
+      country: resJSON.sys.country,
+    });
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getWeather(query);
+  };
+
+  useEffect(() => {
+    getWeather(location);
+  }, [location]);
+
+  return (
+    <div>
+      <WeatherCard
+        temp={weather.temp}
+        condition={weather.condition}
+        city={weather.city}
+        country={weather.country}
+      />
+      <form>
+        <input value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={(e) => handleSearch(e)}>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default WeatherEngine;
